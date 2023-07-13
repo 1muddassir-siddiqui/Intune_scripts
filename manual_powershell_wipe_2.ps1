1e3c765ef1b61f4824488b4b4346310b1764c6c9
Mohammed Muddassir Siddiqui <1muddassir.siddiqui@gmail.com>
2023-07-13 16:37:38 +0530

    Create manual_powershell_wipe_2.ps1

---
+++ manual_powershell_wipe_2.ps1
@@ -0,0 +1,10 @@
+# Prompt the user for confirmation before proceeding
+$confirmation = Read-Host "This action will wipe your PC and restore it to factory settings. Are you sure you want to continue? (Y/N)"
+
+if ($confirmation -eq "Y" -or $confirmation -eq "y") {
+    # Initiate the factory reset
+    Reset-Computer -Confirm -FactoryReset
+    Write-Host "Factory reset initiated. Your PC will now restart and reset to factory settings."
+} else {
+    Write-Host "Factory reset cancelled."
+}
